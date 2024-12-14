e82cd4ff146bd5a71707452c22515efacd22f991
Alexander Vong <145505974+a1ex0304@users.noreply.github.com>
2024-12-14 01:06:55 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import wordleGuess
+
+
+print("If a team has not won a Championship then it is set as 0")
+print("Colour 1-3 are ranked on the most prominent colour on the team+\n")
+wordleGuess.setupLists("C:/Users/Alex Vong/PycharmProjects/wordleSideProject/sportwordle.csv")
+wordleGuess.wordleGuessing()
